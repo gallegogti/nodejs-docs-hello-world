03cd27e3970c109dce4432efe7a79b85eb7cd084
gallegogti <gallegogti@gmail.com>
2022-04-22 17:18:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World FG!");
+    response.end("Hello World FDS!");
 });
 
 const port = process.env.PORT || 1337;
